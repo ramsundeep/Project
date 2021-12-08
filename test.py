43e7a8fa8168fd7fc27abc9a1e3327b3c4928858
ramsundeep <ramsundeepkoduru@ufl.edu>
2021-12-07 23:01:26 -0500

    project submission

--- test.py
+++ test.py
@@ -1,78 +1,63 @@
-import os
-
-from torch.utils.data import Dataset
-from data_prepare import *
-from Function.Network import *
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader, TensorDataset, Dataset
+from torchvision import datasets, transforms, models # add models to the list
+from torchvision.utils import make_grid
+from sklearn.model_selection import train_test_split
+from PIL import Image
+
 import numpy as np
-from sklearn.metrics import confusion_matrix
-from Function.Get_Result import *
 import matplotlib.pyplot as plt
+get_ipython().run_line_magic('matplotlib', 'inline')
 
 
-class test_dataset(Dataset):
-    def __init__(self):
-        self.images = np.load('train_images.npy') / 255
-        # self.labels = np.load('Labels.npy')
-        self.labels = (np.load('train_labels.npy')).reshape(-1)
-
-    def __getitem__(self, index):
-        return torch.tensor(self.images[index], dtype=torch.float32), torch.tensor(self.labels[index],
-                                                                                   dtype=torch.int64)
-
-    def __len__(self):
-        return len(self.labels)
+import warnings
+warnings.filterwarnings("ignore")
 
 
-def test_data(directory, dataloader_dict):
-    path = os.path.join(directory, 'Results/result/Model.pt')
-    save_path = os.path.join(directory, 'Results/test_results')
-    if not os.path.exists(save_path):
-        os.makedirs(save_path)
+class ConvolutionalNetwork(nn.Module):
+    def _init_(self):
+        super()._init_()
+        self.conv1 = nn.Conv2d(1,10, 3, 1)
+        self.conv2 = nn.Conv2d(10, 20, 3, 1)
+        self.conv3 = nn.Conv2d(20,40, 3, 1)
+        self.fc1 = nn.Linear(17*17*40, 100)
+        self.fc2 = nn.Linear(100, 60)
+        self.fc3 = nn.Linear(60,25)
 
-    model = torch.load(path)
-    running_corrects = 0
-    model = model.cpu()
-    GT = np.array(0)
-    Prediction = np.array(0)
-    model.eval()
-    n = 0
-    with torch.no_grad():
-        for test, label in dataloader_dict['test']:
-            test = torch.unsqueeze(test, dim=1)
+    def forward(self, X):
+        X = F.relu(self.conv1(X))
+        X = F.max_pool2d(X, 2, 2)
+        X = F.relu(self.conv2(X))
+        X = F.max_pool2d(X, 2, 2)
+        X = F.relu(self.conv3(X))
+        X = F.max_pool2d(X, 2, 2)
+        X = X.view(-1, 17*17*40)
+        X = F.relu(self.fc1(X))
+        X = F.relu(self.fc2(X))
+        X = self.fc3(X)
+        return F.log_softmax(X, dim=1)
 
-            outputs = model(test)
-            _, preds = torch.max(outputs, 1)
-
-            GT = np.concatenate((GT, label.detach().cpu().numpy()), axis=None)
-
-            Prediction = np.concatenate((Prediction, preds.detach().cpu().numpy()), axis=None)
-            running_corrects += torch.sum(preds == label.data)
-
-            n += label.shape[0]
-
-        test_acc = running_corrects.double() / n
-        print("Accuracy:{:4f}".format(test_acc))
-    np.save(os.path.join(save_path, 'truth_label'), GT[1:])
-    np.save(os.path.join(save_path, 'prediction_label'), Prediction[1:])
-
-    confusion = confusion_matrix(GT[1:], Prediction[1:])
-
-    classes = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16',
-               '17', '18', '19', '20', '21', '22', '23', '24', '25']
-    title = 'Test Confusion Matrix'
-
-    plot_confusion_matrix(confusion, classes, title=title)
-    plt.savefig(os.path.join(save_path, 'cm_acc_' + str(test_acc.item()) + '.png'))
-    plt.show()
-
-    return GT[1:], Prediction[1:]
+mdl="model.pth"    
+model=ConvolutionalNetwork()
+model.load_state_dict(torch.load(mdl))
+model.eval()
 
 
-if __name__ == '__main__':
-    test_set = test_dataset()
-    dataloaders_dict = {'test': torch.utils.data.DataLoader(test_set, batch_size=batch_size['test'],
-                                                            shuffle=True, num_workers=0)}
-    directory = os.getcwd()
-    GT, Prediction = test_data(directory, dataloaders_dict)
+def test_func(X):
+    X=X.reshape(X.shape[0],1,150,150)
+    X=X/255
+    X_testTensor = torch.Tensor(X)
+    y_pred=model(X_testTensor)
+    predicted = torch.max(y_pred.data, 1)[1]
+    predicted=predicted.numpy()
+    return predicted
+
+
+X = np.load("Images.npy")
+y_train=np.load('Labels.npy')
+predicted=test_func(X)
+correct=(predicted == y_train).sum()
+accuracy=correct/y_train.shape*100
+print(accuracy)
